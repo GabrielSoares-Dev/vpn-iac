26f481da69a199a49f91b2ba913ab5e2c0ab65ce
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-05-10 16:27:27 -0300

    feat: create instance

--- ami-openvpn.tf
+++ ami-openvpn.tf
@@ -3,6 +3,6 @@ data "aws_ami" "open_vpn_ami" {
 
   filter {
     name   = "name"
-    values = ["ami-0ddda618e961f2270 (64 bits (x86), uefi-preferred) / ami-09d190d2c3c5b16e6 (64 bits (Arm), uefi)"]
+    values = ["ami-0a36d74f29e8a3ee9"]
   }
 }
