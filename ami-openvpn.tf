d750f9f0a856f185baef93be2af1117dac0ef209
Gabriel Soares Maciel <gabriel.maciel@terceirizadoraizen.com>
2024-05-10 17:13:29 -0300

    feat: create instance

--- ami-openvpn.tf
+++ ami-openvpn.tf
@@ -1,8 +1,17 @@
 data "aws_ami" "open_vpn_ami" {
   most_recent = true
-
   filter {
     name   = "name"
-    values = ["ami-0a36d74f29e8a3ee9"]
+    values = ["OpenVPN Access Server Community Image-fe8020db-5343-4c43-9e65-5ed4a825c931"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
   }
 }
