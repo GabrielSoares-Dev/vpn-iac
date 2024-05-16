6d6777dc87c992d544887ec49aef34517c66047d
Gabriel Soares Maciel <gsm2015@Outlook.com.br>
2024-05-15 21:16:04 -0300

    feat: add role

--- instance.tf
+++ instance.tf
@@ -1,10 +1,10 @@
 resource "aws_instance" "openvpn" {
-  ami           = data.aws_ami.open_vpn_ami.id
-  instance_type = "t2.micro"
+  ami                         = data.aws_ami.open_vpn_ami.id
+  instance_type               = "t2.micro"
   associate_public_ip_address = true
-  security_groups = [aws_security_group.security_group_openvpn.id]
-  subnet_id = var.subnet_id
-  iam_instance_profile = aws_iam_instance_profile.openvpn_instance_profile.name
+  security_groups             = [aws_security_group.security_group_openvpn.id]
+  subnet_id                   = var.subnet_id
+  iam_instance_profile        = aws_iam_instance_profile.openvpn_instance_profile.name
 
   tags = {
     Name = "openvpn"
--- role.tf
+++ role.tf
@@ -1,51 +1,22 @@
 resource "aws_iam_role" "openvpn_instance_role" {
   name = "openvpn_instance_role"
   assume_role_policy = jsonencode({
-    Version = "2012-10-17",
+    Version = "2012-10-17"
     Statement = [
-        {
-            Effect = "Allow",
-            Action = [
-                "ssm:DescribeAssociation",
-                "ssm:GetDeployablePatchSnapshotForInstance",
-                "ssm:GetDocument",
-                "ssm:DescribeDocument",
-                "ssm:GetManifest",
-                "ssm:GetParameter",
-                "ssm:GetParameters",
-                "ssm:ListAssociations",
-                "ssm:ListInstanceAssociations",
-                "ssm:PutInventory",
-                "ssm:PutComplianceItems",
-                "ssm:PutConfigurePackageResult",
-                "ssm:UpdateAssociationStatus",
-                "ssm:UpdateInstanceAssociationStatus",
-                "ssm:UpdateInstanceInformation"
-            ],
-        },
-        {
-            Effect = "Allow",
-            Action = [
-                "ssmmessages:CreateControlChannel",
-                "ssmmessages:CreateDataChannel",
-                "ssmmessages:OpenControlChannel",
-                "ssmmessages:OpenDataChannel"
-            ],
-        },
-        {
-            Effect = "Allow",
-            Action = [
-                "ec2messages:AcknowledgeMessage",
-                "ec2messages:DeleteMessage",
-                "ec2messages:FailMessage",
-                "ec2messages:GetEndpoint",
-                "ec2messages:GetMessages",
-                "ec2messages:SendReply"
-            ],
-            Resource = "*"
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "ec2.amazonaws.com"
         }
+      },
     ]
   })
 
 }
 
+resource "aws_iam_role_policy_attachment" "openvpn_instance_ssm_policy" {
+  role       = aws_iam_role.openvpn_instance_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
